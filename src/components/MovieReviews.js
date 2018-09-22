91498ef46cabec8c7d9391d8b66832cefa1de93b
chejianchao <820992376@qq.com>
2018-09-22 16:15:57 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,23 @@
 // Code MovieReviews Here
+
+import React from 'react'
+
+
+const Review = ({headline,link,summary_short})=>{
+  return (
+    <div key={summary_short} className="review">
+      <a href={link.url} target="_blank"><h3>{headline}</h3></a>
+      <div>{summary_short}</div>
+    </div>
+  )
+}
+
+const MoiveReviews = ({reviews})=>{
+  return (
+    <div className="review-list">
+      {reviews.map(Review)}
+    </div>
+  )
+}
+
+export default MoiveReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -3,7 +3,44 @@ import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
+
 // Code SearchableMovieReviewsContainer Here
+
+export default class SearchableMovieReviewsContainer extends Component {
+
+  constructor(props){
+    super(props)
+    this.state={
+      searchTerm:"",
+      reviews:[]
+    }
+  }
+
+  handleChange = (event)=>this.setState({searchTerm:event.target.value})
+  handleSubmit = (event)=>{
+    event.preventDefault();
+    fetch(URL+`&query=${this.state.searchTerm}`)
+      .then(resp=>resp.json())
+      .then(json=>{
+        this.setState({
+          reviews:json.results
+        })
+      })
+  }
+  render(){
+    return (
+      <div className="searchable-movie-reviews">
+
+        <form onSubmit={this.handleSubmit} >
+        searchTerm:<input type="text" onChange={this.handleChange}/>
+        <input type="submit" onSubmit={this.handleSubmit}/>
+        </form>
+        <h2>The Search Reviews:</h2>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+}
