eede01dfc52bf5fb5340a8d25c3ce797e7619837
gardiner nardini <gardiner.nardini@gmail.com>
2018-09-23 23:59:00 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,4 +6,27 @@ const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends React.Component{
+    state = {
+        reviews:[]
+    }
+
+    render(){
+        return(
+            <div className='latest-movie-reviews'>
+            <MovieReviews reviews={this.state.reviews}/>
+            </div>
+        )
+    }
+
+    componentDidMount(){
+        fetch(URL).
+        then(res=>res.json()).
+        then(nytObj=>{
+            this.setState({reviews:nytObj.results})
+        })
+    }
+
+}
+
+export default LatestMovieReviewsContainer
